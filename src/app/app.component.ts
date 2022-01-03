d152108d834439cef266154a0bd1d54796d55734
Simon Kihara <symokihara@gmail.com>
2022-01-03 16:12:13 +0300

    Consuming a github api using angular HttpClient Module and RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+export class Repos {
+  id: string = '';
+  name: string = '';
+  html_url: string= '';
+  description: string = '';
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'consumingapi';
+export class AppComponent implements OnInit {
+  userName: string = "medic";
+  baseURL: string = "https://api.github.com/";
+  repos: Repos[]= [];
+
+  constructor(private http:HttpClient){
+
+  }
+
+  ngOnInit(){
+      this.getRepos();
+  }
+
+  public getRepos() {
+    return this.http.get<Repos[]>(this.baseURL+'users/' + this.userName + '/repos')
+    .subscribe(
+      (response) => {
+        console.log('Response Received');
+        console.log(response);
+        this.repos = response;
+
+      },
+      (error) => {
+        console.error("Request Failed");
+        alert('error')
+      },
+      ()=> {
+        console.log("Request completed");
+      }
+    )
+  }
 }
